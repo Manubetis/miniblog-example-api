d02888adc039a660641bfeba4b013ac84c5b977f
Manubetis <alum.mjaner.2021@iesaixar.org>
2023-01-30 12:18:47 +0100

    Conexión MongoDB Atlas

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,12 +27,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,5 +41,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var mongoose = require('mongoose');
+mongoose.set('strictQuery', false); //requerido para quitar el warning
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
 
 module.exports = app;
